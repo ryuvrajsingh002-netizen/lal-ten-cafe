101136b017cc5cf93662873ec174a6cb6d62c862
ryuvrajsingh002-netizen <ryuvrajsingh002@gmail.com>
2026-09-07 19:50:15 +0530

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
-    plugins: [react(), tailwindcss()],
+    base: '/lal-ten-cafe/',
+    plugins: [react(), tailwindcss()],,
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
